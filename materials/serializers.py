bae46b1ed9f7c1b36246bebc5662c053ac8c4317
Artem <baza.a1@bk.ru>
2024-06-19 19:38:38 +0300

    Забыл импортировать Lesson

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework.serializers import ModelSerializer
-from materials.models import Course
+from materials.models import Course, Lesson
 
 
 class CourseSerializer(ModelSerializer):
