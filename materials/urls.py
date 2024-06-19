e4a3fe9b223cfe9cb66ae6b541baeb3f7b4ba45e
Artem <baza.a1@bk.ru>
2024-06-19 19:56:31 +0300

    Настроили урлы для модели урок

--- materials/urls.py
+++ materials/urls.py
@@ -1,12 +1,26 @@
 from rest_framework.routers import SimpleRouter
-from materials.views import CourseViewSet
+from materials.views import (
+    CourseViewSet,
+    LessonCreateApiView,
+    LessonListApiView,
+    LessonRetrieveApiView,
+    LessonUpdateAPIView,
+    LessonDestroyAPIView,
+)
 from materials.apps import MaterialsConfig
+from django.urls import path
 
 app_name = MaterialsConfig.name
 
 router = SimpleRouter()
 router.register("", CourseViewSet)
 
-urlpatterns = []
+urlpatterns = [
+    path("lessons/create/", LessonCreateApiView.as_view(), name="lessons_create"),
+    path("lessons/", LessonListApiView.as_view(), name="lessons_list"),
+    path("lessons/<int:pk>/", LessonRetrieveApiView.as_view(), name="lessons_retrieve"),
+    path("lessons/<int:pk>/update/", LessonUpdateAPIView.as_view(), name="lessons_update"),
+    path("lessons/<int:pk>/delete/", LessonDestroyAPIView.as_view(), name="lessons_destroy"),
+]
 
 urlpatterns += router.urls
