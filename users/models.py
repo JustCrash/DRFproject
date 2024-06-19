6221a9a4e4404334e284603bf2e93d8abb61b479
Artem <baza.a1@bk.ru>
2024-06-19 17:32:39 +0300

    Модель пользователь

--- users/models.py
+++ users/models.py
@@ -1,4 +1,36 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {"blank": True, "null": True}
+
 
 class User(AbstractUser):
-    pass
+    username = None
+    email = models.EmailField(
+        unique=True, verbose_name="Почта", help_text="Укажите почту"
+    )
+    phone = models.CharField(
+        max_length=35,
+        verbose_name="Телефон",
+        help_text="Ввведите номер телефона",
+        **NULLABLE
+    )
+    city = models.CharField(
+        max_length=50, verbose_name="Город", help_text="Кажите город", **NULLABLE
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatars",
+        verbose_name="Аватарка",
+        help_text="Загрузите аватарку",
+        **NULLABLE
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
