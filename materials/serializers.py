416b4d76a541341ac9dcc0afea3d25522b70ad8e
Artem <baza.a1@bk.ru>
2024-06-22 16:19:43 +0300

    Привёл вид в порядок

--- materials/serializers.py
+++ materials/serializers.py
@@ -6,31 +6,37 @@ from rest_framework import serializers
 class LessonSerializer(ModelSerializer):
     class Meta:
         model = Lesson
-        fields = '__all__'
+        fields = "__all__"
 
 
 class CourseSerializer(ModelSerializer):
-    information_all_lessons = LessonSerializer(many=True, source='course')
+    information_all_lessons = LessonSerializer(many=True, source="course")
     number_of_lessons = serializers.SerializerMethodField(read_only=True)
-
-    class Meta:
-        model = Course
-        fields = ['id', 'title', 'description', 'number_of_lessons', 'information_all_lessons']
 
     def get_number_of_lessons(self, course):
         return Lesson.objects.filter(course=course).count()
 
     def create(self, validated_data):
-        lesson = validated_data.pop('information_all_lessons')
+        lesson = validated_data.pop("information_all_lessons")
         course = Course.objects.create(**validated_data)
         for lesson in lesson:
             Lesson.objects.create(course=course, **lesson)
             return course
 
+    class Meta:
+        model = Course
+        fields = [
+            "id",
+            "title",
+            "description",
+            "number_of_lessons",
+            "information_all_lessons",
+        ]
+
 
 class CourseDitailSerializer(serializers.ModelSerializer):
     number_of_lessons = serializers.SerializerMethodField(read_only=True)
-    information_all_lessons = LessonSerializer(many=True, source='course')
+    information_all_lessons = LessonSerializer(many=True, source="course")
     subscription = serializers.SerializerMethodField(read_only=True)
 
     def get_number_of_lessons(self, course):
@@ -38,4 +44,10 @@ class CourseDitailSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Course
-        fields = ['id', 'title', 'description', 'number_of_lessons', 'information_all_lessons']
+        fields = [
+            "id",
+            "title",
+            "description",
+            "number_of_lessons",
+            "information_all_lessons",
+        ]
