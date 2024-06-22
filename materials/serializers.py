90a6b5933c8b8e1303eaa9761544a7b468618c5c
Artem <baza.a1@bk.ru>
2024-06-22 16:04:33 +0300

    Для модели курса добавьте в сериализатор поле вывода количества уроков. Поле реализуйте с помощью SerializerMethodField()

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,14 +1,41 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from materials.models import Course, Lesson
-
-
-class CourseSerializer(ModelSerializer):
-    class Meta:
-        model = Course
-        fields = '__all__'
+from rest_framework import serializers
 
 
 class LessonSerializer(ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+
+
+class CourseSerializer(ModelSerializer):
+    information_all_lessons = LessonSerializer(many=True, source='course')
+    number_of_lessons = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Course
+        fields = ['id', 'title', 'description', 'number_of_lessons', 'information_all_lessons']
+
+    def get_number_of_lessons(self, course):
+        return Lesson.objects.filter(course=course).count()
+
+    def create(self, validated_data):
+        lesson = validated_data.pop('information_all_lessons')
+        course = Course.objects.create(**validated_data)
+        for lesson in lesson:
+            Lesson.objects.create(course=course, **lesson)
+            return course
+
+
+class CourseDitailSerializer(serializers.ModelSerializer):
+    number_of_lessons = serializers.SerializerMethodField(read_only=True)
+    information_all_lessons = LessonSerializer(many=True, source='course')
+    subscription = serializers.SerializerMethodField(read_only=True)
+
+    def get_number_of_lessons(self, course):
+        return Lesson.objects.filter(course=course).count()
+
+    class Meta:
+        model = Course
+        fields = ['id', 'title', 'description', 'number_of_lessons', 'information_all_lessons']
